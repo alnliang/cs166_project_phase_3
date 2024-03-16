de9b4865b89ac09765a8e21d4b98060401d2d6c4
CharlesEOhagin <charles.ohagin@gmail.com>
2024-03-16 14:21:10 -0700

    checking for bugs

--- java/src/Amazon.java
+++ java/src/Amazon.java
@@ -409,7 +409,9 @@ public class Amazon {
       try{
          System.out.print("\tEnter Store ID: ");
          String storeID = in.readLine();
+         System.out.print(storeID);
          String query = String.format ("SELECT p.productname, p.numberofunits, p.priceperunit FROM product p WHERE p.storeid = storeID",storeID);
+         System.out.print(query);
          esql.executeQueryAndPrintResult(query);
       }
       catch(Exception e){
